d3bbea4c78efa771044289627ecd52099ebab4b1
marcos <mamanimarcos113@gmail.com>
2025-01-09 16:42:38 -0400

    feat: dependencies for class validator

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
@@ -12,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     database: 'login',
     entities: [__dirname + '*/**/.entity{.ts,.js}'],
     synchronize: true
-})],
+}), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
